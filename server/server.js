4d79238e66b93113f04f272d23dc67fce76cb78b
Bala Konda Reddy D <balakonda.d@gmail.com>
2017-11-21 19:00:51 +0530

    [Access Control]

--- server/server.js
+++ server/server.js
@@ -18,6 +18,13 @@ var PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.post('/todos', authenticate, (req, res) => {
 	console.log(req.body);
 	var todo = new Todo({
